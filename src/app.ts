c720e82a263fe00ea1c7c8f5bd1834c97def6320
Nissan Barua <Baruanissan55@gmail.com>
2025-08-09 02:53:46 +0600

    Revert "Part 1"

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,11 @@
 import express, { Request, Response } from "express";
-import cors from "cors";
-import { router } from "./routes";
-import { globalErrorHandler } from "./app/middlewares/globalMiddleWareHandler";
-import notFound from "./app/middlewares/notFound";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-// API ENDPOINTS
-app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Welcome To Tour Management database",
   });
 });
-
-// errorHandler
-app.use(globalErrorHandler);
-
-// not found route
-app.use(notFound);
 
 export default app;
--- src/app/modules/user/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { userController } from "./user.controller";
-
-const router = Router();
-
-router.post("/register", userController.createUser);
-router.get("/all-users", userController.getAllUsers);
-
-export const UserRoute = router;
