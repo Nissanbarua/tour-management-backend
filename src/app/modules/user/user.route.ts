7ebc8357e6b6bf80a3eaf6d8ff82d923a75f6d50
Nissan Barua <baruanissan55@gmail.com>
2025-08-17 01:45:59 +0600

    Zod validation schema created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,37 +1,12 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { userController } from "./user.controller";
-import z from "zod";
+import { createZodUserSchema } from "./user.validation";
+import { validateRequest } from "../../middlewares/validateRequest";
 
 const router = Router();
-
 router.post(
   "/register",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const createZodUserSchema = z.object({
-      name: z
-        .string({ error: "name must be string" })
-        .min(2, { message: "Name too short" })
-        .max(50, { message: "Name too long" }),
-      email: z.string().email(),
-      password: z
-        .string()
-        .min(8)
-        .regex(
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).+$/,
-          "Password must contain at least one uppercase letter, one lowercase letter, and one special character"
-        ),
-      phone: z
-        .string()
-        .regex(
-          /^(?:\+880|880|0)1[3-9]\d{8}$/,
-          "Invalid Bangladeshi phone number"
-        )
-        .optional(),
-    });
-    req.body = await createZodUserSchema.parseAsync(req.body);
-    next();
-  },
-
+  validateRequest(createZodUserSchema),
   userController.createUser
 );
 router.get("/all-users", userController.getAllUsers);
