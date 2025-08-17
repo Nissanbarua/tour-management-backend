f017892ee0f9b08912150453de47f85d2718c343
Nissan Barua <baruanissan55@gmail.com>
2025-08-18 01:44:31 +0600

    adding password and email password base api creted

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,13 +1,26 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/appError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
+  const { email, ...rest } = payload;
+  const isUserExist = await User.findOne({ email });
+
+  if (isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User already Exist");
+  }
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
 
   const user = await User.create({
-    name,
     email,
+    auths: [authProvider],
+    ...rest,
   });
+
   return user;
 };
 
