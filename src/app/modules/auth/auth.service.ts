281cb14e5a8f8128739daceb5b705e1c3b0d7f34
Nissan Barua <baruanissan55@gmail.com>
2025-08-21 03:15:31 +0600

    created jwt access token

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { IUser } from "../user/user.interface";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import { User } from "../user/user.model";
+import jwt from "jsonwebtoken";
 
 const credentialLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
@@ -21,9 +22,18 @@ const credentialLogin = async (payload: Partial<IUser>) => {
   if (!isPasswordMatched) {
     throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
   }
+  const jwtPayload = {
+    userId: isUserExist._id,
+    email: isUserExist.email,
+    role: isUserExist.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, "secret", {
+    expiresIn: "1d",
+  });
 
   return {
-    email: isUserExist.email,
+    accessToken,
   };
 };
 
