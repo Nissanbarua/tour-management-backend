c5a4d150b7f1f7678e8433057639b61cac617098
Nissan Barua <baruanissan55@gmail.com>
2025-09-02 00:28:10 +0600

    added refresh and access token in cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,11 +3,22 @@ import { catchAsync } from "../../utils/catchAsync";
 import httpstatus from "http-status-codes";
 import { sendRespone } from "../../utils/sendResponse";
 import { AuthServices } from "./auth.service";
+import AppError from "../../errorHelpers/appError";
 
 const credentialLogin = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialLogin(req.body);
+
+    res.cookie("accessToken", loginInfo.accessToken, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
 
     sendRespone(res, {
       success: true,
@@ -21,6 +32,14 @@ const getNewAccessToken = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      throw new AppError(
+        httpstatus.BAD_REQUEST,
+        "No refresh token found in cookie"
+      );
+    }
+
     const tokenInfo = await AuthServices.getNewAccessToken(refreshToken);
 
     sendRespone(res, {
