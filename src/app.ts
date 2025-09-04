b9413aa6ff12396ae2f81aecb3510bf563a27a78
Nissan Barua <baruanissan55@gmail.com>
2025-09-04 23:38:11 +0600

    configure passport js and set env files

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,20 @@ import { router } from "./routes";
 import { globalErrorHandler } from "./app/middlewares/globalMiddleWareHandler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 
 const app = express();
 
+app.use(
+  expressSession({
+    secret: "your Secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
