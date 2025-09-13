b2656fec4373ab4a90bcdf2ae09735f6316f70ff
Nissan Barua <baruanissan55@gmail.com>
2025-09-14 01:58:29 +0600

    Zod error handle

--- src/app/middlewares/globalMiddleWareHandler.ts
+++ src/app/middlewares/globalMiddleWareHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import AppError from "../errorHelpers/appError";
@@ -12,8 +13,47 @@ export const globalErrorHandler = (
 ) => {
   let statusCode = 500;
   let message = "Somthing Went Wrong!!";
+  const errorSource: any = [
+    // {
+    //   path: "isdeleted",
+    //   message: "cast Failed",
+    // },
+  ];
 
-  if (err instanceof AppError) {
+  if (err.code === 1100) {
+    // console.log("Duplicate error", err.message);
+    const matchedArray = err.message.match(/"([^"]*)"/);
+    statusCode = 400;
+    message = `${matchedArray[1]} already exist`;
+  } else if (err === "CastError") {
+    statusCode = 400;
+    message = "Invalid Mongodb Object id. Please provide valid ID";
+  }
+  // zod error
+  else if (err === " ZodError") {
+    statusCode = 400;
+    message = "Zod error";
+    // console.log(err.issues);
+    err.issues.array.forEach((issue: any) => {
+      errorSource.push({
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      });
+    });
+  }
+  // mongoose error
+  else if (err === "ValidationError") {
+    statusCode = 400;
+    const errors = Object.values(err.errors);
+
+    errors.forEach((errorObject: any) =>
+      errorSource.push({
+        path: errorObject.path,
+        message: errorObject.message,
+      })
+    );
+    message = "Validation Error";
+  } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   } else if (err instanceof Error) {
@@ -24,7 +64,8 @@ export const globalErrorHandler = (
   res.status(statusCode).json({
     success: false,
     message: message,
-    err,
+    // err,
+    errorSource,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
 };
